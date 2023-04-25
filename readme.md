4f3a8141bb11aaec7d1e02f6552cfde78872c356
jhbaik1501 <jhbaik1501@gmail.com>
2023-04-25 21:25:10 +0900

    Feat: readme 추가 및 release시 사용할 shell script 변경

---
+++ readme.md
@@ -0,0 +1,3 @@
+## 처음 시작하는 경우
+chmod +x first-release.sh
+sh first-release.sh
--- release-process.sh
+++ release-process.sh
@@ -1,3 +1,3 @@
-sudo docker exec my-custom-nginx-container nginx -s reload
-sudo docker-compose up --no-deps --build web1
-sudo docker-compose up --no-deps --build web2
+sudo docker exec backend-nginx nginx -s reload
+sudo docker compose up --no-deps --build web1 -d
+sudo docker compose up --no-deps --build web2 -d
