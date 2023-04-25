fd25432aee6ff31f074a994aca3f2130bdf59b30
jhbaik1501 <jhbaik1501@gmail.com>
2023-04-25 21:16:17 +0900

    Fix: 리눅스 환경에서 실행하기 위한 sudo문 추가

--- release-process.sh
+++ release-process.sh
@@ -1,3 +1,3 @@
-docker exec my-custom-nginx-container nginx -s reload
-docker-compose up --no-deps --build web1
-docker-compose up --no-deps --build web2
+sudo docker exec my-custom-nginx-container nginx -s reload
+sudo docker-compose up --no-deps --build web1
+sudo docker-compose up --no-deps --build web2
