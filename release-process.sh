a8720197c5b37a105f772edd5be79c963e709049
jhbaik1501 <jhbaik1501@gmail.com>
2023-04-25 22:22:12 +0900

    Fix: docker compose에서 pull하는 로직 추가

--- release-process.sh
+++ release-process.sh
@@ -1,2 +1,3 @@
 sudo docker exec backend-nginx nginx -s reload
-sudo docker compose up --force-recreate --build -d web1 web2
+sudo docker compose pull web1 web2
+sudo docker compose up --force-recreate -d web1 web2
