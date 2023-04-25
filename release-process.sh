7b13cbb01548bf2d6eda81a37b1e564900c1d7a5
jhbaik1501 <jhbaik1501@gmail.com>
2023-04-25 22:04:34 +0900

    fix: 변경된 코드가 바뀌지 않는 오류 fix

--- release-process.sh
+++ release-process.sh
@@ -1,3 +1,2 @@
 sudo docker exec backend-nginx nginx -s reload
-sudo docker compose up --no-deps --build web1 -d
-sudo docker compose up --no-deps --build web2 -d
+sudo docker-compose up --force-recreate --build -d web1 web2
